2f4470d82f04f10217cb677ece4f53d9898df4ed
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-22 12:01:14 +0530

    Modify origin

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-const express = require('express');
+const express = require("express");
 const fetch = require("cross-fetch");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: '*'
-}))
+app.use(
+  cors({
+    origin: "https://devfoliomoonman369.netlify.app/",
+  })
+);
 
-app.get('/',(req,resp)=>{
-    resp.status(404).json({"status":"error","message":"please enter your username (eg: https://leetcode-api.cyclic.app/moonman369)"})
-})
+app.get("/", (req, resp) => {
+  resp.status(404).json({
+    status: "error",
+    message:
+      "please enter your username (eg: https://leetcode-api.cyclic.app/moonman369)",
+  });
+});
 
-app.get('/:username',async (req,resp)=>{
-
-        try{
-            const LEETCODE_API_ENDPOINT = 'https://leetcode.com/graphql'
-            const DAILY_CODING_CHALLENGE_QUERY = `
+app.get("/:username", async (req, resp) => {
+  try {
+    const LEETCODE_API_ENDPOINT = "https://leetcode.com/graphql";
+    const DAILY_CODING_CHALLENGE_QUERY = `
                 {    
                     allQuestionsCount { difficulty count }
                         matchedUser(username: "${req.params.username}") {
@@ -29,43 +34,50 @@ app.get('/:username',async (req,resp)=>{
                         }
                     }
                 }
-                `
-        
-                const init = {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
-                }
-            
-                const response = await fetch(LEETCODE_API_ENDPOINT, init)
-                const data = await response.json()
+                `;
 
-            const obj = 
-            {
-                "status": "success",
-                "totalSolved": data['data']['matchedUser']['submitStats']['acSubmissionNum'][0]['count'],
-                "totalQuestions": data['data']['allQuestionsCount'][0]['count'],
-                "easySolved": data['data']['matchedUser']['submitStats']['acSubmissionNum'][1]['count'],
-                "totalEasy": data['data']['allQuestionsCount'][1]['count'],
-                "mediumSolved": data['data']['matchedUser']['submitStats']['acSubmissionNum'][2]['count'],
-                "totalMedium": data['data']['allQuestionsCount'][2]['count'],
-                "hardSolved": data['data']['matchedUser']['submitStats']['acSubmissionNum'][3]['count'],
-                "totalHard": data['data']['allQuestionsCount'][3]['count'],
-                "ranking": data['data']['matchedUser']['profile']['ranking'],
-                "contributionPoints": data['data']['matchedUser']['contributions']['points'],
-                "reputation": data['data']['matchedUser']['profile']['reputation'],
-            }
-            resp.status(200).json(obj)
-        }
-        
-        catch(e){
-            resp.status(404).json({"status":"error","message":"Username Not Found"})
-        }
-})
+    const init = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
+    };
 
-app.listen(port,()=>{
-    console.log(`Server running on port: ${port}`);
-})
+    const response = await fetch(LEETCODE_API_ENDPOINT, init);
+    const data = await response.json();
 
+    const obj = {
+      status: "success",
+      totalSolved:
+        data["data"]["matchedUser"]["submitStats"]["acSubmissionNum"][0][
+          "count"
+        ],
+      totalQuestions: data["data"]["allQuestionsCount"][0]["count"],
+      easySolved:
+        data["data"]["matchedUser"]["submitStats"]["acSubmissionNum"][1][
+          "count"
+        ],
+      totalEasy: data["data"]["allQuestionsCount"][1]["count"],
+      mediumSolved:
+        data["data"]["matchedUser"]["submitStats"]["acSubmissionNum"][2][
+          "count"
+        ],
+      totalMedium: data["data"]["allQuestionsCount"][2]["count"],
+      hardSolved:
+        data["data"]["matchedUser"]["submitStats"]["acSubmissionNum"][3][
+          "count"
+        ],
+      totalHard: data["data"]["allQuestionsCount"][3]["count"],
+      ranking: data["data"]["matchedUser"]["profile"]["ranking"],
+      contributionPoints:
+        data["data"]["matchedUser"]["contributions"]["points"],
+      reputation: data["data"]["matchedUser"]["profile"]["reputation"],
+    };
+    resp.status(200).json(obj);
+  } catch (e) {
+    resp.status(404).json({ status: "error", message: "Username Not Found" });
+  }
+});
 
-
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
