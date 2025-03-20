8bfeeb5e8e493ee42b5f4ffca3b7dd65c0fd92d0
Ayan Maiti <mightyayan369@gmail.com>
2025-03-21 02:50:33 +0530

    Add CORS middleware for specific origin and methods

--- app.js
+++ app.js
@@ -220,6 +220,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://moonman.in");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.get("/api/v1", (req, resp) => {
